a2673757b63e79ad1e0b88317a0ef83561b9da40
unknown <jdraxler@DMD13008210D7.insp.com>
2015-08-04 12:24:58 -0400

    adding spinner

--- web/js/app.js
+++ web/js/app.js
@@ -12,6 +12,8 @@ $('.scheduler-button').on('click', function (e) {
 	$(".scheduler-button").removeClass('btn-success');
 	$(this).addClass('btn-success');
 	
+	$("#results-well").find("#results pre").html('<span class="glyphicon glyphicon-refresh glyphicon-spin" style="font-size:32px;"></span> ');
+	
 	// all this should do is call the curl action and pass a var to determine the correct action to take
 	$.ajax({
 		url: 'http://'+ document.domain +'/site/curl',
