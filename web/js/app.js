8fe9dbbd7e9a8db0d95aebdb3ea106c9e653a6ba
jdraxler <jdraxler@gmail.com>
2016-03-21 23:07:10 -0400

    trying diff player

--- web/js/app.js
+++ web/js/app.js
@@ -37,22 +37,21 @@ $("body").on("click", "a.episode_link", function(){
 
     var player= `
         <h3>${title}</h3>
-        <!-- ngIf: freebeeLoaded -->
-        <limelightplayer style="" class="" media="${id}" ng-if="freebeeLoaded">
-            <a style="border:0;clip:rect(0 0 0 0);display:block;height:1px;margin:-1px;outline:none;overflow:hidden;padding:0;position:absolute;width:1px;" title="Flash start" tabindex="-1" href="#limelight_player_45041" id="beforeswfanchor0"></a>
-            <object tabindex="0" 
-            type="application/x-shockwave-flash" autoplay="true" id="limelight_player_45041" name="limelight_player_45041" 
-            class="LimelightEmbeddedPlayerFlash swfPrev-beforeswfanchor0 swfNext-afterswfanchor0" 
-            data="//video.limelight.com/player/loader.swf" height="537px" width="100%"> 
-                <param name="movie" value="https://video.limelight.com/player/loader.swf"> 
-                <param name="wmode" value="window"> 
-                <param name="allowScriptAccess" value="always">
-                <param name="allowFullScreen" value="true">
-                <param name="flashVars" value="playerForm=fc40dbd8074e41dcb70d5f47bbf85da1&amp;autoplay=false&amp;autoplayNextClip=false&amp;mediaId=${id}"> 
-            </object>
-            <a style="border:0;clip:rect(0 0 0 0);display:block;height:1px;margin:-1px;outline:none;overflow:hidden;padding:0;position:absolute;width:1px;" title="Flash end" tabindex="-1" href="#limelight_player_45041" id="afterswfanchor0"></a>
-        </limelightplayer>
-        <!-- end ngIf: freebeeLoaded -->
+ 
+        <span class="LimelightEmbeddedPlayer">
+        <script src="http://video.limelight.com/player/embed.js"></script>
+        <object 
+        type="application/x-shockwave-flash" autoplay="true" id="limelight_player_45041" name="limelight_player_45041" 
+        class="LimelightEmbeddedPlayerFlash swfPrev-beforeswfanchor0 swfNext-afterswfanchor0" 
+        data="//video.limelight.com/player/loader.swf" height="537px" width="100%"> 
+        <param name="movie" value="http://video.limelight.com/player/loader.swf"/>
+        <param name="wmode" value="window"/>
+        <param name="allowScriptAccess" value="always"/>
+        <param name="allowFullScreen" value="true"/>
+        <param name="flashVars" value="playerForm=fc40dbd8074e41dcb70d5f47bbf85da1&amp;autoplay=false&amp;autoplayNextClip=false&amp;mediaId=${id}"/>
+        </object>
+        <script>LimelightPlayerUtil.initEmbed('limelight_player_95243');</script>
+        </span>
     `;
 
     $("#Video").html(player);
